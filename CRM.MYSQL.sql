250b0496d97e28858e947ddfec4b880293cb6e86
luizzz <140329178+cmkdheyfgk@users.noreply.github.com>
2024-06-21 09:34:55 -0300

    Add files via upload

---
+++ CRM.MYSQL.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE CRM;
+USE CRM;
+
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    telefone VARCHAR(255)
+);
+
+select*from CRM
+
+
